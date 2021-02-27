12b87622632abe4b237a671b69c6bee8177697cb
gulce yilmaz <gulceyilmaz00@gmail.com>
2021-02-27 11:48:13 +0300

    27.02.2021

--- WebAPI_/Startup.cs
+++ WebAPI_/Startup.cs
@@ -30,10 +30,10 @@ namespace WebAPI_
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton<ICarService, CarManager>();
-            services.AddSingleton<ICarDal, EfCarDal>();
-            services.AddSingleton<IUserService, UserManager>();
-            services.AddSingleton<IUserDal, EfUserDal>();
+            //services.AddSingleton<ICarService, CarManager>();
+            //services.AddSingleton<ICarDal, EfCarDal>();
+            //services.AddSingleton<IUserService, UserManager>();
+            //services.AddSingleton<IUserDal, EfUserDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
